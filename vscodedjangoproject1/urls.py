72a4053b228555a949a74c89b68cf0bf1cf166d2
StremCipher <47041923+StremCipher@users.noreply.github.com>
2021-10-15 03:11:28 +0530

    working on ui of admin page

--- vscodedjangoproject1/urls.py
+++ vscodedjangoproject1/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 import django
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),

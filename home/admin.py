eadf7df42427851a0392f1f5ddff71a0304c446e
StremCipher <47041923+StremCipher@users.noreply.github.com>
2021-10-18 02:56:37 +0530

    ok

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,3 @@ from django.contrib import admin
 from home.models import Contact
 # Register your models here.
 admin.site.register(Contact)
-# admin page login details:
-# username: amitkumar
-# pass: Amit@123
